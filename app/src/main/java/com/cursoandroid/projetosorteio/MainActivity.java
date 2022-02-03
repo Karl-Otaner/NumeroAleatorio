1692d52bed37a1874fc9163ee12bcded225bc15e
Karl-Otaner <carlosrnsilva01@gmail.com>
2022-02-03 08:09:36 -0300

    exemploApp Randomico

---
+++ app/src/main/java/com/cursoandroid/projetosorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cursoandroid.projetosorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sorteaNumero(View view){
+
+        TextView texto = findViewById(R.id.textoResultado);
+
+        int numero = new Random().nextInt(10);
+
+        texto.setText("Número: " + numero);
+    }
+}
